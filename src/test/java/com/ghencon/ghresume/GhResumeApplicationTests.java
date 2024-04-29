741ec2e185ef993e807fba9d35640b87b5940269
Gustavo <gustavojoseh@gmail.com>
2024-04-29 01:44:29 -0300

    Initial commit

---
+++ src/test/java/com/ghencon/ghresume/GhResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghencon.ghresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhResumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
